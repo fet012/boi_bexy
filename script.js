a7361e100eb18ad5c6a32a5bb0dd9d4c1f95026b
fet012 <falolachristiana446@gmail.com>
2024-11-18 00:49:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// const backgroundImages = [
+//   "images/ifeoma.jpg",
+//   "images/ifeoma.jpg",
+//   "images/logo.jpg",
+//   "images/meloddy.jpg",
+//   "images/pepe-cover.jpg",
+// ];
+// let currentIndex = 0;
+// setInterval(() => {
+//   const backgroundSlider = document.querySelector(".backgroundSlider");
+//   const newImage = document.createElement("img");
+//   newImage.src = backgroundImages[(currentIndex + 1) % backgroundImages.length];
+//   backgroundSlider.appendChild(newImage);
+
+//   setTimeout(() => {
+//     backgroundSlider.removeChild(backgroundSlider.children[0]);
+//   }, 10000);
+
+//   currentIndex = (currentIndex + 1) % backgroundImages.length;
+// }, 10000);
