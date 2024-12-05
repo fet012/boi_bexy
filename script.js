6394e787c779d6a261694b561acdce3e1514f926
fet012 <falolachristiana446@gmail.com>
2024-12-05 19:17:18 +0100

    update

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-// const images = [
-//     'images/ifeoma.jpg',
-//     'images/kachi.jpg',
-//     'images/logo.jpg',
-//     'images/meloddy.jpg',
-//     'images/pepe-cover.jpg'
-// ];
+gsap.to(".logo", {
+  y: -8, 
+  repeat: -1, 
+  yoyo: true, 
+  duration: 1, 
+  ease: "power1.inOut"
+});
 
-// let currentIndex = 0;
 
-// function changeBackground() {
-//     currentIndex = (currentIndex + 1) % images.length;
-//     document.querySelector('.home-section').style.backgroundImage = `url(${images[currentIndex]})`;
-// }
 
-// setInterval(changeBackground, 5000);
+gsap.from(".song-title", {
+  duration: 2,
+  opacity: 0,
+  scale: 0.5,
+  ease: "back.out(1.7)",
+  stagger: 0.1,
+});
+
+gsap.utils.toArray(".icons svg").forEach(icon => {
+  icon.addEventListener("mouseenter", () => {
+    gsap.to(icon, { scale: 1.2, rotation: 15, duration: 0.4 });
+  });
+  icon.addEventListener("mouseleave", () => {
+    gsap.to(icon, { scale: 1, rotation: 0, duration: 0.4 });
+  });
+});
+
+
+gsap.from(".navbar-nav .nav-item", {
+  duration: 0.5,
+  x: -100,
+  opacity: 0,
+  stagger: 0.1,
+  ease: "power2.out"
+});
+
+
+
+gsap.from(".logo path", {
+  duration: 2,
+  strokeDashoffset: 1000,
+  strokeDasharray: 1000,
+  ease: "power2.inOut"
+});
+
+gsap.registerPlugin(SplitText);
+const splitText = new SplitText(".song-title", { type: "chars" });
+gsap.from(splitText.chars, {
+  duration: 0.8,
+  opacity: 0,
+  y: 50,
+  stagger: 0.05,
+  ease: "back.out(1.7)"
+});
+
 
 
 // SMOOTH SCROLL
@@ -33,6 +72,14 @@ window.addEventListener("scroll", () => {
       section.classList.remove("visible");
     }
   });
+});
+
+// JavaScript to toggle the navbar and icon
+const toggleButton = document.getElementById("navbarToggleBtn");
+
+toggleButton.addEventListener("click", function () {
+  // Toggle the collapse class on the button
+  toggleButton.classList.toggle("collapsed");
 });
 
 
