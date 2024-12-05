e11f28119d659c4b4521b28fa4ca04684114d883
fet012 <falolachristiana446@gmail.com>
2024-12-05 13:41:42 +0100

    updates

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const images = [
-    'images/ifeoma.jpg',
-    'images/kachi.jpg',
-    'images/logo.jpg',
-    'images/meloddy.jpg',
-    'images/pepe-cover.jpg'
-];
+// const images = [
+//     'images/ifeoma.jpg',
+//     'images/kachi.jpg',
+//     'images/logo.jpg',
+//     'images/meloddy.jpg',
+//     'images/pepe-cover.jpg'
+// ];
 
-let currentIndex = 0;
+// let currentIndex = 0;
 
-function changeBackground() {
-    currentIndex = (currentIndex + 1) % images.length;
-    document.querySelector('.home-section').style.backgroundImage = `url(${images[currentIndex]})`;
-}
+// function changeBackground() {
+//     currentIndex = (currentIndex + 1) % images.length;
+//     document.querySelector('.home-section').style.backgroundImage = `url(${images[currentIndex]})`;
+// }
 
-setInterval(changeBackground, 5000);
+// setInterval(changeBackground, 5000);
+
+
+// SMOOTH SCROLL
+const scrollSections = document.querySelectorAll(".scroll-section");
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  scrollSections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    if (
+      scrollPosition >= sectionTop - 600 &&
+      scrollPosition < sectionTop + sectionHeight
+    ) {
+      section.classList.add("visible");
+    } else {
+      section.classList.remove("visible");
+    }
+  });
+});
+
+
